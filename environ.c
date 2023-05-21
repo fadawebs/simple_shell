8d4bb1a6014c13a9957b9a44e1a5e5dd7c6c7256
beekaz <sooperfect00@gmail.com>
2023-05-20 17:00:27 -0700

    function to relate to a particular interface of configuration

---
+++ environ.c
@@ -0,0 +1,91 @@
+#include "shell.h"
+
+/**
+ * _getenv - gets value of env_list variable
+ * @info: Potential arguments active.
+ * @name: env variable name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _shellenv - print current env_listment working
+ * @info: Potential arguments active.
+ * Return: Always 0
+ */
+int _shellenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+/**
+ * _shellsetenv - Initialize an env_listment variable,
+ *             or reactor an existing one
+ * @info: Potential arguments active.
+ *  Return: Always 0
+ */
+int _shellsetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Inaccurate number or arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _shellunsetenv - Removes env_listment variable
+ * @info: Potential arguments active.
+ *  Return: Always 0
+ */
+int _shellunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Not enough arguement.\n");
+		return (1);
+	}
+	for (i = 1; i < info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - increases env linked list
+ * @info: Potential arguments active.
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+	char **env_list;
+
+	env_list = NULL;
+
+	for (i = 0; env_list[i]; i++)
+		add_node_end(&node, env_list[i], 0);
+	info->env = node;
+	return (0);
+}
