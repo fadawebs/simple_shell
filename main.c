99be5ce86fc50ae94cf19b959ff77b76658cb4eb
beekaz <sooperfect00@gmail.com>
2023-05-21 19:13:33 -0700

    betty linspacing and trailing whitspaces

--- main.c
+++ main.c
@@ -8,38 +8,38 @@
  */
 int main(int argc, char **argv)
 {
-    info_t info[] = {INFO_INIT};
-    int fd = 2;
+	info_t info[] = {INFO_INIT};
+	int fd = 2;
 
-    asm("mov %1, %0\n\t"
-        "add $3, %0"
-        : "=r"(fd)
-        : "r"(fd));
+	asm("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r"(fd)
+			: "r"(fd));
 
-    if (argc == 2)
-    {
-        fd = open(argv[1], O_RDONLY);
-        if (fd == -1)
-        {
-            if (errno == EACCES)
-                exit(126);
-            if (errno == ENOENT)
-            {
-                _eputs(argv[0]);
-                _eputs(": 0: No Access ");
-                _eputs(argv[1]);
-                _eputchar('\n');
-                _eputchar(BUF_FLUSH);
-                exit(127);
-            }
-            return (EXIT_FAILURE);
-        }
-        info->readfd = fd;
-    }
-    populate_env_list(info);
-    read_history(info);
-    hsh(info, argv);
-    return (EXIT_SUCCESS);
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(argv[0]);
+				_eputs(": 0: No Access ");
+				_eputs(argv[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, argv);
+	return (EXIT_SUCCESS);
 }
 /**
  * add - function for addition of int
@@ -49,7 +49,7 @@ int main(int argc, char **argv)
  */
 int add(int y, int x)
 {
-    return (y + x);
+	return (y + x);
 }
 
 float multiply(float a, float b);
@@ -61,5 +61,5 @@ float multiply(float a, float b);
  */
 float multiply(float a, float b)
 {
-    return (a * b);
+	return (a * b);
 }
