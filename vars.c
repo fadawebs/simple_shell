be713b87dd17c58e957eaf2375ba062dd34a6a3a
beekaz <sooperfect00@gmail.com>
2023-05-23 23:35:03 -0700

    betty checked

--- vars.c
+++ vars.c
@@ -10,27 +10,26 @@
  * Return: Void
  */
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
-
 {
-    size_t j = *p;
+	size_t j = *p;
 
-    if (info->cmd_buf_type == CMD_AND)
-    {
-        if (info->status)
-        {
-            buf[i] = 0;
-            j = len;
-        }
-    }
-    if (info->cmd_buf_type == CMD_OR)
-    {
-        if (!info->status)
-        {
-            buf[i] = 0;
-            j = len;
-        }
-    }
-    *p = j;
+	if (info->cmd_buf_type == CMD_AND)
+	{
+		if (info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+	if (info->cmd_buf_type == CMD_OR)
+	{
+		if (!info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+	*p = j;
 }
 
 /**
@@ -41,35 +40,34 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
  */
 int replace_vars(info_t *info)
 {
-    int i = 0;
-    list_t *node;
+	int i = 0;
+	list_t *node;
 
-    for (i = 0; info->argv[i]; i++)
-    {
-        if (info->argv[i][0] != '$' || !info->argv[i][1])
-            continue;
-        if (!_strcmp(info->argv[i], "$?"))
-        {
-            replace_string(&(info->argv[i]),
-                           tbtb_strdup(convert_number(info->status, 10, 0)));
-            continue;
-        }
-        if (!_strcmp(info->argv[i], "$$"))
-        {
-            replace_string(&(info->argv[i]),
-                           tbtb_strdup(convert_number(getpid(), 10, 0)));
-            continue;
-        }
-        node = node_starts_with(info->env, &info->argv[i][1], '=');
-        if (node)
-        {
-            replace_string(&(info->argv[i]),
-                           tbtb_strdup(_strchr(node->str, '=') + 1));
-            continue;
-        }
-        replace_string(&info->argv[i], tbtb_strdup(""));
-    }
-    return (0);
+	for (i = 0; info->argv[i]; i++)
+	{
+		if (info->argv[i][0] != '$' || !info->argv[i][1])
+			continue;
+		if (!_strcmp(info->argv[i], "$?"))
+		{
+			replace_string(&(info->argv[i]),
+					tbtb_strdup(convert_number(info->status, 10, 0)));
+			continue;
+		}
+		if (!_strcmp(info->argv[i], "$$"))
+		{
+			replace_string(&(info->argv[i]),
+					tbtb_strdup(convert_number(getpid(), 10, 0)));
+			continue;
+		}
+		node = node_starts_with(info->env, &info->argv[i][1], '=');
+		if (node)
+		{
+			replace_string(&(info->argv[i]), tbtb_strdup(_strchr(node->str, '=') + 1));
+			continue;
+		}
+		replace_string(&info->argv[i], tbtb_strdup(""));
+	}
+	return (0);
 }
 
 /**
@@ -81,9 +79,9 @@ int replace_vars(info_t *info)
  */
 int replace_string(char **old, char *new)
 {
-    free(*old);
-    *old = new;
-    return (1);
+	free(*old);
+	*old = new;
+	return (1);
 }
 
 /**
@@ -94,25 +92,25 @@ int replace_string(char **old, char *new)
  */
 int replace_alias(info_t *info)
 {
-    int i;
-    list_t *node;
-    char *p;
+	int i;
+	list_t *node;
+	char *p;
 
-    for (i = 0; i < 10; i++)
-    {
-        node = node_starts_with(info->alias, info->argv[0], '=');
-        if (!node)
-            return (0);
-        free(info->argv[0]);
-        p = _strchr(node->str, '=');
-        if (!p)
-            return (0);
-        p = tbtb_strdup(p + 1);
-        if (!p)
-            return (0);
-        info->argv[0] = p;
-    }
-    return (1);
+	for (i = 0; i < 10; i++)
+	{
+		node = node_starts_with(info->alias, info->argv[0], '=');
+		if (!node)
+			return (0);
+		free(info->argv[0]);
+		p = _strchr(node->str, '=');
+		if (!p)
+			return (0);
+		p = tbtb_strdup(p + 1);
+		if (!p)
+			return (0);
+		info->argv[0] = p;
+	}
+	return (1);
 }
 
 /**
@@ -125,27 +123,27 @@ int replace_alias(info_t *info)
  */
 int is_chain(info_t *info, char *buf, size_t *p)
 {
-    size_t j = *p;
+	size_t j = *p;
 
-    if (buf[j] == '|' && buf[j + 1] == '|')
-    {
-        buf[j] = 0;
-        j++;
-        info->cmd_buf_type = CMD_OR;
-    }
-    else if (buf[j] == '&' && buf[j + 1] == '&')
-    {
-        buf[j] = 0;
-        j++;
-        info->cmd_buf_type = CMD_AND;
-    }
-    else if (buf[j] == ';') /* end of this command */
-    {
-        buf[j] = 0; /* semicolon with null */
-        info->cmd_buf_type = CMD_CHAIN;
-    }
-    else
-        return (0);
-    *p = j;
-    return (1);
+	if (buf[j] == '|' && buf[j + 1] == '|')
+	{
+		buf[j] = 0;
+		j++;
+		info->cmd_buf_type = CMD_OR;
+	}
+	else if (buf[j] == '&' && buf[j + 1] == '&')
+	{
+		buf[j] = 0;
+		j++;
+		info->cmd_buf_type = CMD_AND;
+	}
+	else if (buf[j] == ';') /* end of this command */
+	{
+		buf[j] = 0; /* semicolon with null */
+		info->cmd_buf_type = CMD_CHAIN;
+	}
+	else
+		return (0);
+	*p = j;
+	return (1);
 }
