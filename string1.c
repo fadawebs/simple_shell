0fa4da74aa23ca54553fb7ad8f91a1b50e8e611d
beekaz <sooperfect00@gmail.com>
2023-05-23 23:35:30 -0700

    checked

--- string1.c
+++ string1.c
@@ -4,46 +4,51 @@
 #include <unistd.h>
 /**
  * tb_strdup - duplicates a string
- *
+ * main -entry point
  * This function duplicates the input string and returns a pointer to the new
  * string. The memory for the new string is allocated dynamically using malloc.
  *
  * @str: The input string to be duplicated
  * return: A pointer to the duplicated string on success, or NULL on failure
  */
-/**
- * char *tb_strdup(const char *str);
-int main(void)
-{
+
+
+/* char *tb_strdup(const char *str); */
+
+/* int main(void) */
+/* { */
+
+/*
     const char *str = "Hello, world!";
     char *new_str = tb_strdup(str);
     printf("Original: %s\n", str);
     printf("Copy:     %s\n", new_str);
     free(new_str);
-    return 0;
+    return (0);
 }
 */
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 #define WRITE_BUF_SIZE 1024
 int _putchar(char c)
 {
-    static int i = 0;
-    static char buf[WRITE_BUF_SIZE];
-    if (c == '\0' || i >= WRITE_BUF_SIZE - 1)
-    {
-        buf[i] = '\0';
-        write(STDOUT_FILENO, buf, i);
-        i = 0;
-    }
-    if (c != '\0')
-        buf[i++] = c;
-    return 1;
+	static int i = 0;
+	static char buf[WRITE_BUF_SIZE];
+	if (c == '\0' || i >= WRITE_BUF_SIZE - 1)
+	{
+		buf[i] = '\0';
+		write(STDOUT_FILENO, buf, i);
+		i = 0;
+	}
+	if (c != '\0')
+		buf[i++] = c;
+	return (1);
 }
 /**
  * _strcpy - copies a string
@@ -54,16 +59,17 @@ int _putchar(char c)
  */
 char *_strcpy(char *dest, const char *src)
 {
-    int i = 0;
-    if (dest == src || src == NULL)
-        return dest;
-    while (src[i])
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return dest;
+	int i = 0;
+
+	if (dest == src || src == NULL)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 /**
  * _puts - prints an input string
@@ -73,22 +79,34 @@ char *_strcpy(char *dest, const char *src)
  */
 void _puts(const char *str)
 {
-    int i = 0;
-    if (!str)
-        return;
-    while (str[i])
-    {
-        _putchar(str[i]);
-        i++;
-    }
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
 }
 
+/**
+ * tbtb_strdup - duplicates a string
+ * This function duplicates the input string and returns a pointer to the new
+ * string. The memory for the new string is allocated dynamically using malloc.
+ *
+ * @str: The input string to be duplicated
+ * Return: A pointer to the duplicated string on success, or NULL on failure
+ */
 char *tbtb_strdup(const char *str)
 {
-    size_t len = strlen(str) + 1;
-    char *new_str = malloc(len);
-    if (new_str == NULL)
-        return NULL;
-    memcpy(new_str, str, len);
-    return new_str;
+	size_t len = strlen(str) + 1;
+	char *new_str = malloc(len);
+
+	if (new_str == NULL)
+	{
+		return NULL;
+	}
+	memcpy(new_str, str, len);
+	return (new_str);
 }
